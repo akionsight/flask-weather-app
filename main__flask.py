faf34c15a9c52594328491823b54e1d78873aa71
Harsh Pandey <akionsight@gmail.com>
2020-09-17 00:15:36 +0530

    major changes

--- main__flask.py
+++ main__flask.py
@@ -1,27 +1,30 @@
 # Weather App using Flask
 
 ## imports 
-import random
+import secrets
 import main
-import string
+import requests
 from flask import Flask
 from flask import render_template
 from flask import request
 from flask import redirect
 from flask import url_for
 from flask import session
+
+no_internet = False
 app = Flask(__name__)
-secret_key = ''
-for i in range(5):
-    letter = random.choice(list(string.ascii_letters))
-    secret_key = secret_key + letter
-app.secret_key = secret_key
+app.secret_key = secrets.token_urlsafe(32)
+
 ## initalisation
 @app.route('/', methods=["GET", "POST"])
 def main_page():
     if request.method == "POST":
         city = request.form["city"]
-        wee = main.get_weather_final(city)
+        try:
+            wee = main.get_weather_final(city)
+        except requests.exceptions.ConnectionError:
+            no_internet = True
+            return redirect(url_for('no_internet'))
         session['city'] = wee['for_city']
         session['temp_faren'] = wee['temp_faren']
         session['temp_celcius'] = wee['temp_celcius']
@@ -32,9 +35,17 @@ def main_page():
 
 @app.route('/weather/')      
 def give_weather():
-    return render_template('weather.html', city=session['city'], temp_faren=session['temp_faren'], temp_celcius=session['temp_celcius'], humidity=session['humidity'])
-
-
-
+    try:
+        return render_template('weather.html', city=session['city'], temp_faren=session['temp_faren'], temp_celcius=session['temp_celcius'], humidity=session['humidity'])
+    except KeyError:
+        return redirect(url_for('main_page'))
+@app.route('/no_internet/')
+def no_internet():
+    if no_internet == True:
+        return render_template('please_check_connection.html')
+    else:
+        return redirect(url_for('main_page'))
+        pass
+        
 if __name__ == "__main__":
     app.run(debug=True)
